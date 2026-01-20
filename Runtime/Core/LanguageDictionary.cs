54baf33e7843df02cff2d58ec994297d37fa8a86
thief01 <pst12345@wp.pl>
2026-01-20 01:34:11 +0100

    get language data

--- Runtime/Core/LanguageDictionary.cs
+++ Runtime/Core/LanguageDictionary.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using PandaTranslator.Runtime.Components;
 using PandaTranslator.Runtime.Core.Interfaces;
 using PandaTranslator.Runtime.Data;
+using UnityEngine;
 
 namespace PandaTranslator.Runtime.Core
 {
@@ -10,7 +11,7 @@ namespace PandaTranslator.Runtime.Core
         private readonly Dictionary<string, LanguageItem> items;
         private LanguageSettings languageSettings;
         private Language currentLanguage;
-        
+
         public LanguageDictionary(LanguageSettings languageSettings)
         {
             items = new Dictionary<string, LanguageItem>();
@@ -25,7 +26,7 @@ namespace PandaTranslator.Runtime.Core
                 var keys = category.Keys;
                 foreach (var key in keys)
                 {
-                    var languagePath = category.Name + "/" + key;
+                    var languagePath = GetLanguagePath(category.Name, key);
                     items.Add(languagePath, new LanguageItem());
                 }
             }
@@ -36,34 +37,56 @@ namespace PandaTranslator.Runtime.Core
             currentLanguage = language;
             UpdateTranslations();
         }
+
         public LanguageItem GetLanguageItem(string category, string key)
         {
-            return null;
+            var path = GetLanguagePath(category, key);
+            return GetLanguageItem(path);
         }
 
         public LanguageItem GetLanguageItem(int categoryId, int keyId)
         {
-            return null;
+            var path = GetLanguagePath(categoryId, keyId);
+            return items[path];
         }
 
         public LanguageItem GetLanguageItem(LanguageVariable languageVariable)
         {
-            return null;
+            var path = GetLanguagePath(languageVariable.Category, languageVariable.Key);
+            return GetLanguageItem(path);
         }
 
         public LanguageItem GetLanguageItem(string path)
         {
+            if (items.TryGetValue(path, out var item))
+            {
+                return item;
+            }
+
+            Debug.LogError($"LanguageDictionary.GetLanguageItem: {path} path not found");
             return null;
         }
 
+        private string GetLanguagePath(int categoryId, int keyId)
+        {
+            var languageDefinitionData = languageSettings.LanguageDefinitionData;
+            var category = languageDefinitionData.Categories[categoryId];
+            var key = category.Keys[keyId];
+            return GetLanguagePath(category.Name, key);
+        }
 
+        private string GetLanguagePath(string category, string key)
+        {
+            return category + "/" + key;
+        }
+        
         private void UpdateTranslations()
         {
             foreach (var languageCategory in currentLanguage.languageCategories)
             {
                 foreach (var languageItem in languageCategory.languageItems)
                 {
-                    var temp = GetLanguageItem(languageCategory.categoryName,  languageItem.key);
+                    var temp = GetLanguageItem(languageCategory.categoryName, languageItem.key);
                     temp.UpdateFromLanguageData(languageItem);
                 }
             }
